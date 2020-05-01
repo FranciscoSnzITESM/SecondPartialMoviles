89924cbcdc2a60f10afe47ccc9e01f0f0a72029b
FranciscoSnzITESM <hss_francisco@hotmail.com>
2020-05-01 13:57:31 -0600

    toolbar and drawermenu

--- app/src/main/java/mx/itesm/secondpartial/MainActivity.java
+++ app/src/main/java/mx/itesm/secondpartial/MainActivity.java
@@ -1,14 +1,81 @@
 package mx.itesm.secondpartial;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureNavigationDrawer();
+        configureToolbar();
+    }
+
+    private void configureNavigationDrawer() {
+        drawerLayout =  findViewById(R.id.drawer_layout);
+        NavigationView navView =  findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Fragment f = null;
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.refresh) {
+                    //f = new RefreshFragment();
+                } else if (itemId == R.id.stop) {
+                   // f = new StopFragment();
+                }
+                if (f != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame, f);
+                    transaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    private void configureToolbar() {
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_launcher_foreground);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        switch(itemId) {
+            // Android home
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+                // manage other entries if you have it ...
+        }
+        return true;
     }
 }
